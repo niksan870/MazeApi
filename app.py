2f4a4fe53a4c9f5d785c62ee2c82dd3ecd256d80
Nikola Pashov <niksan870@gmail.com>
2022-07-13 00:49:42 +0300

    Update prod app

--- app.py
+++ app.py
@@ -32,9 +32,11 @@ def create_app(config_path="config.DevConfig", drop_db=False):
     return app
 
 
+app = create_app()
+prod_app = create_app()
+
 if __name__ == "__main__":
     # setting debug to True enables hot reload
     # and also provides a debugger shell
     # if you hit an error while running the server
-    app = create_app("config.ProdConfig")
     app.run(debug=True)
